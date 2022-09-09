c201e10367e8ce4d00a755c638478dc340fc76c4
kongheng <kongheng.long14@gmail.com>
2022-09-10 00:32:41 +0700

    jwt

---
+++ src/main/java/com/kongheng/springboot/model/JwtRequest.java
@@ -0,0 +1,14 @@
+package com.kongheng.springboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtRequest {
+
+  private String userName;
+  private String password;
+}
